fde1a0dde9bcd8388909b88a1648c435886bfcf2
hs <35157375@qq.com>
2018-07-01 11:50:37 +0800

    hide login feature

--- components/topbar.vue
+++ components/topbar.vue
@@ -48,7 +48,7 @@
         <i :class="`el-icon-d-arrow-${isSidebarCollapsed?'right':'left'}`"></i>
       </el-button>
     </div>
-    <div class="fr">
+    <!--<div class="fr">
       <el-button class="avatar" @click="toggleLoginState">
         <i></i> {{hasToken?'注销':'登录'}}
       </el-button>
@@ -60,7 +60,7 @@
         <input type="text" v-model="loginDialog.password" placeholder="密码" @keyup.enter="login">
         <input type="button" class="bg-primary" value="登录" @click="login">
       </div>
-    </div>
+    </div>-->
   </div>
 </template>
 
